7873f6d77678f16095be3b816b3a0cb412cd787b
ValentinNM <57604614+ValentinNM@users.noreply.github.com>
2021-08-31 12:45:26 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>{/* Ṛest of components will go here
+        
+         */}</main>
+    </>
+  );
+}
